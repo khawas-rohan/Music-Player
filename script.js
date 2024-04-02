1bc69e82f72d0e656aa4db2e62cb3c95aaed6574
Rohan Khawas <khawasrohan29@gmail.com>
2024-04-02 18:37:09 +0545

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function changeBoxColors(box) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
